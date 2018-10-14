ccca8937c1c134ba4c4ddf8cf14ad501955d69ba
ravindra rawat <ravinrawat.wd@gmail.com>
2018-10-13 18:10:48 -0600

    command line app using yargs

--- fs/app.js
+++ fs/app.js
@@ -1,11 +1,69 @@
+console.log("Starting app.js ....");
+
 const fs = require("fs");
 const os = require("os");
 
-const user = os.userInfo();
-console.log(user);
+const _ = require("lodash");
+const yargs = require("yargs");
 
-// fs.appendFile("greeting.txt", "hello world!!", err => {
-//   if (err) {
-//     console.log(err);
-//   }
-// });
+const notes = require("./notes");
+
+const titleOptions = {
+  description: "Title of note",
+  demand: true,
+  alias: "t"
+};
+
+const bodyOptions = {
+  description: "Body of note",
+  demand: true,
+  alias: "b"
+};
+
+const input = yargs
+  .command("add", "Add a note", {
+    title: titleOptions,
+    body: bodyOptions
+  })
+  .command("list", "List all notes")
+  .command("read", "Read a note", {
+    title: titleOptions
+  })
+  .command("remove", "Remove a note", {
+    title: titleOptions
+  })
+  .help().argv;
+const command = input._[0];
+
+switch (command) {
+  case "add":
+    let note = notes.add(input.title, input.body);
+    if (note)
+      console.log(
+        `Note created : "Title" - "${note.title}", "Body" - "${note.body}"`
+      );
+    else console.log("note already exists");
+    break;
+  case "list":
+    let currentNotes = notes.list();
+    currentNotes = currentNotes.map(note => `${note.title} - ${note.body}`);
+    console.log(currentNotes);
+
+    break;
+  case "read":
+    let recievedNote = notes.read(input.title);
+    let message = recievedNote
+      ? `Note : ${recievedNote.title} - ${recievedNote.body}`
+      : "Note not found";
+    console.log(message);
+    break;
+  case "remove":
+    const noteRemoved = notes.remove(input.title);
+    let removeMessage = noteRemoved ? "Note was removed" : "Note not found";
+    console.log(removeMessage);
+    break;
+
+  default:
+    console.log("Command not recognized...");
+    break;
+}
