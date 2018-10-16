a24741fde010f482b7941599da50cff4ce1de1d2
ravindra rawat <ravinrawat.wd@gmail.com>
2018-10-15 21:25:29 -0600

    middleware

--- express-server/server.js
+++ express-server/server.js
@@ -1,16 +1,43 @@
 require("dotenv").config();
 const express = require("express");
+const hbs = require("hbs");
+const fs = require("fs");
 
 const port = process.env.PORT || 3005;
 
 const app = express();
 app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
+hbs.registerHelper("getCurrentYear", () => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper("screamText", text => {
+  return text.toUpperCase();
+});
+
 app.use(express.static(__dirname + "/public"));
+
+//Middleware
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = `${now},${req.method}, ${req.url}`;
+  fs.appendFile("server.log", log + "\n", err => {
+    if (err) {
+      console.log("Unable to append to server.log");
+    }
+  });
+  console.log(log);
+  next();
+});
+
+// Maintenance middleware
+// app.use((req, res, next) => {
+//   res.render("maintenance.hbs");
+// });
 
 app.get("/", (req, res) => {
   res.render("home.hbs", {
     pageTitle: "Home Page",
-    currentYear: new Date().getFullYear(),
     welcomeMessage: "Welcome home !!!!"
   });
 });
@@ -24,8 +51,7 @@ app.get("/info", (req, res) => {
 
 app.get("/about", (req, res) => {
   res.render("about.hbs", {
-    pageTitle: "About Page",
-    currentYear: new Date().getFullYear()
+    pageTitle: "About Page"
   });
 });
 
